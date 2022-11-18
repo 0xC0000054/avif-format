db1e5d59df4e15c83faf70b446be90f4ac11c311
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-11-17 20:10:10 -0700

    Bump version to 1.0.6.3 for a test build

--- src/common/version.h
+++ src/common/version.h
@@ -21,7 +21,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VI_VERSION       1,0,6,2
-#define VI_VERSION_STR  "1.0.6.2"
+#define VI_VERSION       1,0,6,3
+#define VI_VERSION_STR  "1.0.6.3"
 
 #endif
